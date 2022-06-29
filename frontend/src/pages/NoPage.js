4c5a29bad670aeb1d939679979a1018ea465a3e3
schmkls <mickels.martin@gmail.com>
2022-06-29 17:22:51 +0200

    testing gitignore

--- frontend/src/pages/NoPage.js
+++ frontend/src/pages/NoPage.js
@@ -1,18 +1,40 @@
 import React from "react";
 import Globals from '../globals/Globals.css';
 import usePages from './usePages'
+import useSecrets from "../useSecrets";
+import Axios from 'axios';
+
+
+const AUTH_URL = ''
 
 const NoPage = () => {
 
-    const pagesObj = usePages();
-    const pages = pagesObj.pages;
+    var client_id = '00ec5f716a774d7ba0fd58833ec22323'; // Your client id
+    var client_secret = 'a5544978b6314f4cab9efc731152862d'; // Your secret
+
+    // your application requests authorization
+    var authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+        },
+        form: {
+        grant_type: 'client_credentials'
+        },
+        json: true
+    };
+
+    const getToken = () => {
+        Axios.get().then(() => {
+
+        }).catch((err) => {console.log(err)});
+    }
 
 
 
     return (
         <div className='margin-top'>
             <h2>Not a page</h2>
-            <a href={pages.library.fullUrl}>Go home</a>
         </div>
     );
 }
