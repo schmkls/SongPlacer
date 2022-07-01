1ac48bc09d6db8b7b4bacec5b4cb512b0e394e5f
schmkls <mickels.martin@gmail.com>
2022-07-01 03:46:54 +0200

    use env variables for client_id+secret

--- spotify_server/index.js
+++ spotify_server/index.js
@@ -1,18 +1,25 @@
 /**
  * Server handling/providing Spotify services for the client. 
  */
-
 const express = require('express');
 const app = express();
 app.use(express.json());
 
+var wihu = process.env.WIHUTEST;
+console.log("env variable test:"  + wihu);
+
+//environment variables
+//https://medium.com/codait/environment-variables-or-keeping-your-secrets-secret-in-a-node-js-app-99019dfff716
+//https://developer.spotify.com/documentation/general/guides/authorization/code-flow/
+const client_id = process.env.SONGPLACER_CLIENT_ID;
+const client_secret = process.env.SONGPLACER_CLIENT_SECRET;
 
 app.listen(3002, () => {
     console.log("Spotify server running on port 3002");
 });
 
 
-//todo get client_id and client_secret from environment variables 
+//todo get client_id and client_secret from environment variables      
 app.get('/login', function(req, res) {
 
     var state = generateRandomString(16);
